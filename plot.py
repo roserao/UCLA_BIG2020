349ff4bce8dba01913966b8889e2700f0ecb7369
roserao <61214314+roserao@users.noreply.github.com>
2020-07-14 17:16:52 -0700

    sample size20/50/100/200/500

--- plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-if __name__ == '__main__':
-    h2g_hess = np.loadtxt("h2g_hess.txt", delimiter=",")
-    plt.hist(h2g_hess, bins=30)
-    plt.show()
